910eb0266e97f64b4e1b186ae99b1c8f6d7d8e6c
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-12-21 12:12:58 +0100

    installed clerk for signin/signout and configured it with a button on home page

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,32 @@
+import { SignedOut } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={128}
+          height={38}
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full shadow-lg shadow-indigo-500/50" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
   )
 }
 
